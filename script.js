305afbbe6ce185b1c4bc6f1151a66f49d4a428ea
Narmin Molina <narminmolina@Narmins-MacBook-Pro.local>
2022-07-22 22:08:45 -0400

    counter

---
+++ script.js
@@ -0,0 +1,28 @@
+let count = 0;
+
+const value = document.querySelector("#value");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const classList = event.currentTarget.classList;
+
+    if (classList.contains("decrease")) {
+      count--;
+    } else if (classList.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else {
+      value.style.color = "#222";
+    }
+
+    value.innerHTML = count;
+  });
+});
