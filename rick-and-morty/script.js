7c2345920618cdb948efdbcbf32badf49c567909
Narmin Molina <narminmolina@Narmins-MacBook-Pro.local>
2022-08-02 10:07:59 -0400

    Rick-and-Morthy

--- rick-and-morty/script.js
+++ rick-and-morty/script.js
@@ -1,14 +1,5 @@
 const apiURL = "https://rickandmortyapi.com/api/character";
 
-// function getCharacter(character) {
-//   fetch(`${apiURL}`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       // show Data
-//       console.log(data);
-//     });
-// }
-
 async function getCharacter() {
   let res = await fetch(`${apiURL}`);
   let data = await res.json();
@@ -23,16 +14,17 @@ async function getEpisode(parametr) {
   return data;
 }
 
-async function applyCardInfo() {
-  let gridItem = document.querySelector(".grid-item");
+async function applyCardInfoSingle() {
   let image = document.querySelector(".image img");
   let title = document.querySelector("h2");
-  let location = document.querySelector(".location");
   let firstSeen = document.querySelector(".first-seen");
   let statusIcon = document.querySelector(".status-icon");
 
   let { results } = await getCharacter();
-  console.log("🚀 ~ file: script.js ~ line 35 ~ applyCardInfo ~ results", results)
+  console.log(
+    "🚀 ~ file: script.js ~ line 35 ~ applyCardInfo ~ results",
+    results
+  );
   const [firstElement] = results;
 
   //   const [{ name, image: imageSrc, episode: episodes, status }] = results;
@@ -49,6 +41,73 @@ async function applyCardInfo() {
   status === "Alive"
     ? (statusIcon.style.backgroundColor = "green")
     : (statusIcon.style.backgroundColor = "red");
-    
 }
-applyCardInfo();
+
+let container = document.querySelector(".grid-container");
+const more = document.querySelector(".more");
+
+export async function applyCardInfo(results) {
+  results.forEach(async (element) => {
+    const episode = await getEpisode(element.episode[0]);
+
+    container.innerHTML += `
+  <div class="grid-item">
+   <div class="image">
+     <img src=${element.image} alt="photo" width="220px" height="220px" />
+   </div>
+   <div class="description">
+     <h2>${element.name}</h2>
+     <span class="status">
+       <span class="status-icon ${element.status}"></span>
+     </span>
+     <div class="section">
+       <span class="text-gray">Last known location:</span>
+       <a href="" class="location">
+         ${element.location.name}
+       </a>
+     </div>
+     <div class="section">
+       <span class="text-gray">First seen in:</span>
+        <a class="first-seen">${episode.name}</a>
+     </div>
+   </div>
+ </div>`;
+  });
+}
+
+let { results } = await getCharacter();
+
+applyCardInfo(results);
+
+async function getNewCharacters(url) {
+  console.log("🚀 ~ file: script.js ~ line 92 ~ getNewCharacters ~ url", url);
+  let res = await fetch(url);
+  let data = await res.json();
+
+  return data;
+}
+
+async function getMoreCharacters(url) {
+  let { info } = await getCharacter();
+  // const next = await getNewCharacters(info.next);
+  // const prev = await getNewCharacters(info.next);
+
+  if (info.next || info.prev) {
+    more.innerHTML = `
+		     ${
+           info.prev
+             ? `<button class="btn" onclick=""getNewCharacters('${info.prev}')"">Prev</button>`
+             : ""
+         }
+		     ${
+           info.next
+             ? `<button class="btn" onclick="getNewCharacters('${info.next}')">Next</button>`
+             : ""
+         }
+		`;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+getMoreCharacters();
